6523d3f289da73eb223307b76d202105eb052ba1
Alejandro Castro <guardiadelcastro@gmail.com>
2019-05-27 12:26:05 +0100

    added simple queries and mutations

---
+++ src/index.js
@@ -0,0 +1,43 @@
+const { GraphQLServer } = require('graphql-yoga')
+
+let links = [{
+  id: 'link-0',
+  url: 'www.howtographql.com',
+  description: 'Fullstack tutorial for GraphQL'
+}]
+let idCount = links.length
+// 2
+const resolvers = {
+  Query: {
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links,
+    link: (args) => links.map( link => link.id === args.id)
+  },
+  Mutation: {
+    post: (parent, args) => {
+       const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      }
+      links.push(link)
+      return link
+    },
+    updateLink: (parent, args) => {
+      const index = links.findIndex(link => link.id === args.id);
+      links[index].description = args.description;
+      links[index].url = args.url;
+      return links[index];
+    },
+    deleteLink: (parent, args) => {
+      links.splice(links.findIndex(link => link.id === args.id));
+    }
+  },
+}
+
+// 3
+const server = new GraphQLServer({
+  typeDefs: './src/schema.graphql',
+  resolvers,
+})
+server.start(() => console.log(`Server is running on http://localhost:4000`))
